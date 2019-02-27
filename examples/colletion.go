8eb4ad4a8bc04a1099d74b12fa08bd617d2c44ef
caifan <2998000457@qq.com>
2019-02-27 18:32:27 +0800

    数组、切片

--- examples/colletion.go
+++ examples/colletion.go
@@ -4,5 +4,29 @@ import "fmt"
 
 func main(){
 	fmt.Println("数组、切片、字典")
-	
+
+	f0()
+
+	f1()
+}
+
+func f0() {
+	a := [...]string{"a", "b", "c", "d"}
+	for i := range a {
+		fmt.Println("Array item", i, "is", a[i])
+	}
+}
+
+func f1() {
+	arr := new([5]int) //arr实际上是数组首地址指针
+	arr[0] = 0
+	arr[1] = 1
+	arr[2] = 2
+	arr[3] = 3
+	arr[4] = 4
+
+	var arr2 [5]int = *arr
+	arr2[1] = 10
+	fmt.Println(*arr)
+	fmt.Println(arr2)
 }
