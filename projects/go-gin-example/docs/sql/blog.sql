6abafa0e4b711d86dbf6892517903ef91301f517
FCF5646448 <2998000457@qq.com>
2023-05-28 13:02:46 +0800

    优化细节，修改错误

--- projects/go-gin-example/docs/sql/blog.sql
+++ projects/go-gin-example/docs/sql/blog.sql
@@ -34,10 +34,10 @@ DROP TABLE IF EXISTS `blog_tag`;
 CREATE TABLE `blog_tag` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(100) DEFAULT '' COMMENT '标签名称',
-    `create_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
-    `create_by` varchar(100) DEFAULT '' COMMENT '创建人',
+    `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+    `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
     `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间'
-    `modified_y` varchar(100) DEFAULT '' COMMENT '修改人',
+    `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
     `deleted_on` int(10) unsigned DEFAULT '0',
     `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态0为禁用，1为启用',
     PRIMARY KEY (`id`)
